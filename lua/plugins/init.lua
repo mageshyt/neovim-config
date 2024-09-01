c9cc94395a3a34126e454c3e89dfec4e00219433
MAGESH H <70838644+mageshyt@users.noreply.github.com>
2024-09-01 09:03:07 +0530

    chore: Add code-runner configuration for C language

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,10 +1,31 @@
 return {
   {
     "stevearc/conform.nvim",
-    -- event = 'BufWritePre', -- uncomment for format on save
-    opts = require "configs.conform",
+    -- event = 'BufWritePre', -- uncomment for format on save opts = require "configs.conform",
   },
+  {
+    "nvimtools/none-ls.nvim",
+    event = "VeryLazy",
+    opts = function()
+      return require "configs.null-ls"
+end,
+  },
+  -- Code Runner
+  {
+    "CRAG666/code_runner.nvim",
+    config = true,
+    cmd="RunCode",
+    opts={
+      mode="better_term",
+    }
 
+  },
+  {
+    'norcalli/nvim-colorizer.lua',
+    config = function()
+      require('colorizer').setup()
+    end
+  },
   -- These are some examples, uncomment them if you want to see them work!
   {
     "neovim/nvim-lspconfig",
@@ -23,11 +44,14 @@ return {
   opts = {
     -- your config goes here
     -- or just leave it empty :)
-  },
+    },
+
   },
   {
     "mfussenegger/nvim-jdtls",
-  },{
+  },
+  -- Masion
+  {
     "williamboman/mason.nvim",
     opts = {
       ensure_installed ={
@@ -39,6 +63,7 @@ return {
       }
     },
   },
+  -- Auto Tag
   {
     "windwp/nvim-ts-autotag",
     ft={
@@ -52,6 +77,7 @@ return {
       require('nvim-ts-autotag').setup()
     end
   },
+  -- File Tree
   {
   	"nvim-treesitter/nvim-treesitter",
   	opts = {
@@ -61,6 +87,17 @@ return {
         "javascript", "typescript",
         "tsx","go" ,"rust"
   		},
+      
   	},
   },
+  -- {
+  --   "hrsh7th/nvim-cmp",
+  --
+  --   config = function()
+  --     require "configs.cmp"
+  --   end,
+  --
+  -- },
 }
+
+
