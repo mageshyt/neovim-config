058cfb46dd0560e0ee890d6b4aa4d2950e06cc90
MAGESH H <70838644+mageshyt@users.noreply.github.com>
2024-10-06 20:06:34 +0530

    added more plugins
    dressing and indentblack line and ufo for flods

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -3,8 +3,31 @@ require "nvchad.mappings"
 local map = vim.keymap.set
 local keymap = vim.keymap
 
+-- basic configs
+vim.o.foldcolumn='1'
+vim.o.foldlevel=99
+vim.o.foldenable=true
+vim.o.foldlevelstart=99
+
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jj", "<ESC>")
+
+-- ufo set
+vim.keymap.set('n','zR',require('ufo').openAllFolds,{desc = 'Open all folds'})
+vim.keymap.set('n','zM',require('ufo').closeAllFolds,{desc = 'close all folds'})
+keymap.set('n','zK', function ()
+  local winid=require('ufo').peekFoldedLinesUnderCursor()
+  if not winid then
+    vim.lsp.buf.hover()
+  end
+end,{desc = 'Peek fold'}
+)
+
+require('ufo').setup({
+  provider_selector=function (bufnr,filetype,buftype)
+    return {'lsp','indent'}
+  end
+})
 
 
 -- increment/decrement numbers
