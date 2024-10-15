78918058e853eb75971112f3a60c78f0a7e8a967
MAGESH H <70838644+mageshyt@users.noreply.github.com>
2024-10-15 22:51:45 +0530

    nvim option setup

--- lua/options.lua
+++ lua/options.lua
@@ -1,6 +1,26 @@
 require "nvchad.options"
 
 -- add yours here!
+local opt = vim.opt
+
+opt.relativenumber = true
+opt.number = true
+
+
+-- search settings
+opt.ignorecase = true -- ignore case when searching
+opt.smartcase = true -- if you include mixed case in your search, assumes you want case-sensitive
+
+opt.cursorline = true
+
+-- turn on termguicolors for tokyonight colorscheme to work
+-- (have to use iterm2 or any other true color terminal)
+opt.termguicolors = true
+opt.background = "dark" -- colorschemes that can be light or dark will be made dark
+opt.signcolumn = "yes" -- show sign column so that text doesn't shift
+
+-- backspace
+opt.backspace = "indent,eol,start" -- allow backspace on indent, end of line or insert mode start position
 
 -- local o = vim.o
 -- o.cursorlineopt ='both' -- to enable cursorline!
