632d527c15ed194f07bde1b756570387895bb2d2
MAGESH H <70838644+mageshyt@users.noreply.github.com>
2024-10-15 22:52:20 +0530

    dashboard setup done

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -5,24 +5,55 @@
 local M = {}
 
 M.base46 = {
-	theme = "pastelDark",
-
-	-- hl_override = {
-	-- 	Comment = { italic = true },
-	-- 	["@comment"] = { italic = true },
-	-- },
+  theme = "pastelDark",
 }
+M.ui = {
+  statusline = {
+    theme = "vscode_colored", -- default/vscode/vscode_colored/minimal
+    -- default/round/block/arrow separators work only for default statusline theme
+    -- round and block will work for minimal theme only
+    separator_style = "round",
+    modules = nil,
+    order = nil,
+  },
+}
+M.nvdash={
+  load_on_startup=true,
+  header={
+    
+  },
+  buttons = {
+      { txt = "  Find File", keys = "ff", cmd = "Telescope find_files" },
+      { txt = "  Recent Files", keys = "fo", cmd = "Telescope oldfiles" },
+      { txt = "󰈭  Find Word", keys = "fw", cmd = "Telescope live_grep" },
+      { txt = "󱥚  Themes", keys = "th", cmd = ":lua require('nvchad.themes').open()" },
+      { txt = "  Mappings", keys = "ch", cmd = "NvCheatsheet" },
 
+      { txt = "─", hl = "NvDashLazy", no_gap = true, rep = true },
+
+      {
+        txt = function()
+          local stats = require("lazy").stats()
+          local ms = math.floor(stats.startuptime) .. " ms"
+          return "  Loaded " .. stats.loaded .. "/" .. stats.count .. " plugins in " .. ms
+        end,
+        hl = "NvDashLazy",
+        no_gap = true,
+      },
+
+      { txt = "─", hl = "NvDashLazy", no_gap = true, rep = true },
+    },
+}
 M.cpilot = {
   i = {
     ["<C-l>"] = {
       function()
-        vim.fn.feedkeys(vim.fn['copilot#Accept'](), '')
+        vim.fn.feedkeys(vim.fn["copilot#Accept"](), "")
       end,
       "Copilot Accept",
-      {replace_keycodes = true, nowait=true, silent=true, expr=true, noremap=true}
-    }
-  }
+      { replace_keycodes = true, nowait = true, silent = true, expr = true, noremap = true },
+    },
+  },
 }
 
 return M
