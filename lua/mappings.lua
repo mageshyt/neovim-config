5d82b8818a163740789b51ccdea1b425a134e371
MAGESH H <70838644+mageshyt@users.noreply.github.com>
2024-10-15 22:51:54 +0530

    added feature to fold

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -4,7 +4,7 @@ local map = vim.keymap.set
 local keymap = vim.keymap
 
 -- basic configs
-vim.o.foldcolumn='1'
+-- vim.o.foldcolumn='1'
 vim.o.foldlevel=99
 vim.o.foldenable=true
 vim.o.foldlevelstart=99
