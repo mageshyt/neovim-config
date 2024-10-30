a9df5acd181f38f8a58d639b43bdc1b19a38ba52
MAGESH H <70838644+mageshyt@users.noreply.github.com>
2024-10-30 15:10:44 +0530

    telscope setting

--- lua/configs/telescope.lua
+++ lua/configs/telescope.lua
@@ -1,7 +1,24 @@
-return{
+return {
   defaults = {
     file_ignore_patterns = {
-      "node_modules"
-    }
-  }
+      "node_modules",
+    },
+    layout_strategy = "horizontal",
+    layout_config = {
+      preview_width = 0.65,
+      horizontal = {
+        size = {
+          width = "95%",
+          height = "95%",
+        },
+      },
+    },
+    pickers = {
+      find_files = {
+        theme = "dropdown",
+      },
+
+    },
+
+  },
 }
