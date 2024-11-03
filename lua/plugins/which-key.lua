5ad10b274e26bb1fe48a523a1a8749c6d82cddaf
MAGESH H <70838644+mageshyt@users.noreply.github.com>
2024-11-03 19:22:05 +0530

    treesitter issue fixed and added more which key

---
+++ lua/plugins/which-key.lua
@@ -0,0 +1,44 @@
+return {
+  "folke/which-key.nvim",
+  event = "VeryLazy",
+
+  opts = {
+    spec = {
+      {
+        "<leader>g",
+        name = "Git",
+      },
+      {
+        "<leader>c",
+        name = "Code",
+      },
+      {
+        "<leader>f",
+        name = "File",
+      },
+      {
+        "<leader>b",
+        name = "Buffer",
+      },
+      {
+        "<leader>q",
+        name = "Quit",
+      },
+      {
+        "<leader>?",
+        name = "Help",
+      },
+      
+    },
+  },
+
+  keys = {
+    {
+      "<leader>?",
+      function()
+        require("which-key").show { global = false }
+      end,
+      desc = "Buffer Local Keymaps (which-key)",
+    },
+  },
+}
