94092360b02b7b99b3a8461f6e5baa20ce17cb6e
MAGESH H <70838644+mageshyt@users.noreply.github.com>
2024-12-15 09:35:08 +0530

    noice and snack config

--- lua/plugins/notify.lua
+++ lua/plugins/notify.lua
@@ -1,16 +1,20 @@
 return {
-  "rcarriga/nvim-notify",
-  event = "VeryLazy",
-  opts = {
-    module = "notify",
-  },
-  config = function()
-    require("notify").setup {
-      stages = "fade",
-      timeout = 3000,
-      background_colour = "#000000",
-      render="wrapped-compact",
-      top_down=false
-    }
-  end,
+  -- "rcarriga/nvim-notify",
+  -- event = "VeryLazy",
+  -- opts = {
+  --   module = "notify",
+  -- },
+  -- config = function()
+  --   require("notify").setup {
+  --     stages = "fade",
+  --     timeout = 3000,
+  --     background_colour = "#000000",
+  --     render="wrapped-compact",
+  --     top_down=false,
+  --     disable=true
+  --   }
+  --
+  --   -- disable noice
+  --
+  -- end,
 }
