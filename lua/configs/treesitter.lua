2c3b3f7de97b35d245059595923711ca78bf1fc1
MAGESH H <70838644+mageshyt@users.noreply.github.com>
2024-12-15 09:34:51 +0530

    new treesitter config

--- lua/configs/treesitter.lua
+++ lua/configs/treesitter.lua
@@ -1,13 +1,9 @@
-
--- [[ Configure Treesitter ]]
--- See `:help nvim-treesitter`
 require('nvim-treesitter.configs').setup {
-  -- Add languages to be installed here that you want installed for treesitter
   ensure_installed = { 
     'go', 'lua', 'python', 'rust', 'typescript', 'regex', 
     'bash', 'markdown', 'markdown_inline', 'kdl', 'sql', 'org', 'terraform',
     'html', 'css', 'javascript', 'yaml', 'json', 'toml',
-    'prisma', 'vimdoc' , "dockerfile", "gitignore",
+    'prisma', 'vimdoc' , "dockerfile", "gitignore", "tsx",
   },
 
   highlight = { enable = true },
